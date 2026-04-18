a9b9edad0e50a2d1e6b1363d518b63f661953efd
mooldang <your-email@example.com>
2026-04-18 18:29:03 +0900

    2026-04-18 UI 룰렛관리 v3

--- MooldangBot.Domain/Entities/RouletteType.cs
+++ MooldangBot.Domain/Entities/RouletteType.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 namespace MooldangBot.Domain.Entities
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum RouletteType
     {
         Cheese,
